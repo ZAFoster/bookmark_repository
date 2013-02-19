ae11fee9eca9129426a2817249382fbe44323c68
ZAFoster <ZFoster@madisoncollege.edu>
2013-02-18 20:33:33 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookmarkRepository::Application.config.secret_token = '921c68772587e54d4a9cdbb2362575f80775fbc4c58f605108cde409976b22e56ba85abaf13dfec4ea05a92b58fd295a8289602b2d75c7fddf478f2296e5ce71'
